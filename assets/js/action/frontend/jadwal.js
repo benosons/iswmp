d22b1b7c2e5cf86f9917b6c14915e298f5f654ba
benosons <beno.sons@gmail.com>
2022-02-27 23:24:05 +0700

    update jadwal

--- assets/js/action/frontend/jadwal.js
+++ assets/js/action/frontend/jadwal.js
@@ -1,6 +1,44 @@
 console.log('You are running jQuery version: ' + $.fn.jquery);
-$(function () {
-    $('#calendar').evoCalendar({
-        theme: 'Orange Coral'
-    })
+$(function () {    
+    loaddata();
 })
+
+function loaddata(){
+
+    $.ajax({
+        type: 'post',
+        dataType: 'json',
+        url: 'getglobal',
+        data : {
+                param       : 'data_jadwal',
+                type        : 'jadwal',
+        },
+        success: function(result){
+            let code = result.code;
+            let data = result.data;
+            let myEvents = [
+                
+            ]
+
+            for (let index = 0; index < data.length; index++) {
+                myEvents.push(
+                    { 
+                    id: data[index]['id'],
+                    name: data[index]['event'], 
+                    date: data[index]['tanggal'], 
+                    type: "holiday", 
+                    everyYear: true,
+                    color: "#222"
+                }
+                )
+            }
+            
+            console.log(myEvents);
+            $('#calendar').evoCalendar({
+                theme: 'Orange Coral',
+                calendarEvents : myEvents
+            })
+
+        }
+    })
+}
