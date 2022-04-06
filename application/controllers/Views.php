d46637bd2df755a02164bceffd2596508d6e30f4
Beno Sons <beno.sons@gmail.com>
2022-04-06 13:30:03 +0700

    clear cache

--- application/controllers/Views.php
+++ application/controllers/Views.php
@@ -41,6 +41,11 @@ class Views extends CI_Controller {
 			"foto" => $this->foto
 		);
 
+		$this->output->set_header('Last-Modified: ' . gmdate("D, d M Y H:i:s") . ' GMT');
+		$this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+		$this->output->set_header('Pragma: no-cache');
+		$this->output->set_header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+
 	}
 
 	public function index()
